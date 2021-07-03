796669a449fbcc9ad1ad997cf2c5862fbbc13447
Oogii214 <oogii07_10@yahoo.com>
2021-07-03 16:56:38 -0400

    Testing

---
+++ src/test/java/InvitingPractice.java
@@ -0,0 +1,7 @@
+public class InvitingPractice {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("This is practice for collaboration");
+    }
+}
